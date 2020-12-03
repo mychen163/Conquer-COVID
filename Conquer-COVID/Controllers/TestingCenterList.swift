e76e91c52ea14812cfd631a68ab7ebab1dec0ecb
M.y Chen <chen.mengyuan@gmail.com>
2020-12-03 13:57:19 -0800

    wrap up

--- Conquer-COVID/Controllers/TestingCenterList.swift
+++ Conquer-COVID/Controllers/TestingCenterList.swift
@@ -12,7 +12,6 @@ import SwiftyJSON
 class TestingCentersList: UITableViewController {
     
     var testingCentersList: [GooglePlace]?
-    // var detailResultsList:[GooglePlace]?
     var googleApiKey = "AIzaSyBl3AW7yrDq7yCRATsP9-aAreOTQ9fxE68"
     var current_placeId = ""
     var photo_reference: String?
@@ -23,12 +22,6 @@ class TestingCentersList: UITableViewController {
         super.viewDidLoad()
         self.tableView.reloadData()
         
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -101,74 +94,3 @@ class TestingCentersList: UITableViewController {
     }
     
 }
-
-/*
- // Override to support conditional editing of the table view.
- override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
- // Return false if you do not want the specified item to be editable.
- return true
- }
- */
-
-/*
- // Override to support editing the table view.
- override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
- if editingStyle == .delete {
- // Delete the row from the data source
- tableView.deleteRows(at: [indexPath], with: .fade)
- } else if editingStyle == .insert {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
- 
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
- // Return false if you do not want the item to be re-orderable.
- return true
- }
- */
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
-
-//find the detailed information about every testing center
-//       func fetchPlaceDetails(place_id:String,completion:@escaping(()->Void)){
-//           var urlString = "https://maps.googleapis.com/maps/api/place/details/json?place_id=\(place_id)&key=\(googleApiKey)"
-//           urlString = urlString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? urlString
-//
-//           guard let url = URL(string: urlString) else{
-//               return
-//           }
-//        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: { (data,response,error) -> Void in
-//            if let json = try? JSON( data: data!, options: .mutableContainers),let result = json["result"].arrayObject as? [[String: Any]] {
-//                DispatchQueue.main.async {
-//                results.forEach { result in
-//                    let place = GooglePlace(dictionary: result)
-//                        //print("There")
-//                        self.placesArray.append(place)
-//                        self.markerPlace(place:place)
-//                    }
-//                    completion()
-//                }
-//
-//            }
-//        }).resume()
-//  }
-
-
